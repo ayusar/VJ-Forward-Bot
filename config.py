b95d54f042e16f21cd26093333168ab84c2c04fb
ayusar <work38896@gmail.com>
2026-01-07 19:59:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "37891385"))
+    API_HASH = environ.get("API_HASH", "2d14a3ad7a0ea5ec9167fb1623f56fa2")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://royalitybots_db_user:948Ptb7Toysx8cd7@cluster0.o2wmbpm.mongodb.net/?appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "RoyalityBots")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "8420494874"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -27,3 +27,4 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
